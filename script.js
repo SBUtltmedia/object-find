0d5b99d562b57d726cd50ae8dafc080c1fb2dc4b
theawesomerazz <contact@theawesomerazz.com>
2016-09-28 17:29:30 -0400

    Added Assets

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var clickable = [{Name:"Mirror",Text:"Mirror Text"}, {Name:"TV",Text:"TV Text"}, {Name:"Pillow",Text:"Pillow Text"}, {Name:"Bed",Text:"Bed Text"}, {Name:"Table",Text:"Table Text"}];
+var clickable = [{Name:"Mirror",Text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et scelerisque libero. Nunc sed nunc nec nisi mollis vestibulum. Praesent pulvinar magna et condimentum lacinia. Donec tortor felis, fringilla sed ipsum vel, finibus placerat urna. Maecenas consectetur suscipit luctus. Nunc tristique porttitor porta. Donec tincidunt eleifend vehicula. Ut iaculis sed ante a bibendum."}, {Name:"TV",Text:"TV Text"}, {Name:"Pillow",Text:"Pillow Text"}, {Name:"Bed",Text:"Bed Text"}, {Name:"Table",Text:"Table Text"}];
 var lookup = {};
 $(function() {
 
   $("#room").css("font-size","100px");  
     $("#roomSVG").load("img/room.svg", roomSvgLoad  );
-  $("#thoughtbubbleSVG").load("img/thought_bubble.svg", thoughtBubbleSvgLoad  );
+
 });
 
 
@@ -16,33 +16,24 @@ function roomSvgLoad (){
       console.log($(value))     
        $("#" + value.Name).addClass("clickable")
        lookup[value.Name]=index;
-   }
+   })
                  
-                ) 
-   
-    
   console.log("something")  
  $(".clickable").click( function(evt){
-  $("#thoughtbubbleSVG").css({ "left":evt.clientX+ "px", "top": evt.clientY+"px","display":"block" })
-  $("#thoughtbubbleSVG svg").removeClass("thoughtPop");
-   
+  $("#thoughtBubble").css({ "left":evt.clientX+ "px", "top": evt.clientY+"px","display":"block" })
+  element = document.getElementById("thoughtBubble");
+     
+  $("#thoughtBubble").removeClass("thoughtPop");
+     void element.offsetWidth;
     console.log(lookup) 
      var clickedItem = $(evt.target).closest('[id]').attr("id");
     //console.log(evt.clientX) 
-     $("#thoughtbubbleSVG text").text(clickable[lookup[clickedItem]].Text)
-       $("#thoughtbubbleSVG svg").addClass("thoughtPop");
+     $("#thoughtBubble").text(clickable[lookup[clickedItem]].Text)
+       $("#thoughtBubble").addClass("thoughtPop");
  }  ) 
     
     
 }
 
-function  thoughtBubbleSvgLoad (){
-  console.log("something")  
- $("#Mirror").click( function(){
-        
-     
- }  ) 
-    
-    
-}
+
 
