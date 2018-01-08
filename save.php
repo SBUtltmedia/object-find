3982e16767afc6ace141987deab6364aacfe4395
KariBarry <kari.a.barry@stonybrook.edu>
2018-01-08 18:16:13 -0500

    More graphical changes and Treasure chest

--- save.php
+++ save.php
@@ -6,7 +6,8 @@ $fileName =$_POST['room'].".json";
 $JSON=json_decode(file_get_contents($fileName));
 
 foreach($JSON->targets as $value){
-
+unset($value->xValue);
+unset($value->yValue);
 if($value->Name==$_POST['name']){
 $value->Text[$_POST['phase']]=$_POST['text'];
 file_put_contents($fileName,json_encode($JSON));
--- script.js
+++ script.js
@@ -13,12 +13,12 @@ var stop = false;
 var frameCount = 0;
 var nextRoom;
 var currentRoom;
-var startingWidth,startingHeight,aspect;
+var startingWidth, startingHeight, aspect;
 var phaseText = ["Find the Triggers", "Ways to avoid the triggers:"]
 var phase = getParameterByName("phase");
 
 if (!phase) {
-	phase = 0
+  phase = 0
 }
 $('#phaseNum').html(phaseText[phase]);
 
@@ -26,143 +26,144 @@ $('#phaseNum').html(phaseText[phase]);
 
 var fps, fpsInterval, startTime, now, then, elapsed;
 $(window).resize(function() {
-	resizeScreen();
+  resizeScreen();
 });
 
 function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 $(function() {
-	currentRoom = getParameterByName("room");
-	if (!currentRoom) {
-		currentRoom = "bedRoom";
-	}
+  currentRoom = getParameterByName("room");
+  if (!currentRoom) {
+    currentRoom = "bedRoom";
+  }
 
-	loadNewRoom(currentRoom);
+  loadNewRoom(currentRoom);
 
-	resizeScreen();
+  resizeScreen();
 
 });
 
 
 //setTimeout(resizeScreen,'200');																		//	For IE
 function resizeScreen() {
-	var w = $(window).width();
-	var h = $(window).height();
+  var w = $(window).width();
+  var h = $(window).height();
 
-	// If the aspect ratio is greater than or equal to 4:3, fix height and set width based on height
-	if ((w / h) >=aspect) {
-			stageHeight = h;
-			stageWidth = (aspect) * h;
-			stageLeft = (w - stageWidth) / 2;
-	        stageTop = 0;
-	}
-	// If the aspect ratio is less than 4:3, fix width and set height based on width
-	else {
-			stageWidth = w;
-			stageHeight = (aspect) * w;
-			stageTop = (h - stageHeight) / 2;
-	 stageLeft = 0;
-	}
+  // If the aspect ratio is greater than or equal to 4:3, fix height and set width based on height
+  if ((w / h) >= aspect) {
+    stageHeight = h;
+    stageWidth = (aspect) * h;
+    stageLeft = (w - stageWidth) / 2;
+    stageTop = 0;
+  }
+  // If the aspect ratio is less than 4:3, fix width and set height based on width
+  else {
+    stageWidth = w;
+    stageHeight = (aspect) * w;
+    stageTop = (h - stageHeight) / 2;
+    stageLeft = 0;
+  }
 
-	// Set "screen" object width and height to stageWidth and stageHeight, and center screen
-	$("#screen").css({
-			width: stageWidth+ "px",
-			height: stageHeight + "px",
-			left: stageLeft + "px",
-			top: stageTop + "px"
-	});
+  // Set "screen" object width and height to stageWidth and stageHeight, and center screen
+  $("#screen").css({
+    width: stageWidth + "px",
+    height: stageHeight + "px",
+    left: stageLeft + "px",
+    top: stageTop + "px"
+  });
 
-	// $("#roomSVG > svg").attr({
-	// 	'width': $("html").width() + 'px',
-	// 	'height': $("html").width() / 1.6 + 'px'
-	// }); //	For IE
-	//console.log($("html").width())
-	$("html").css('fontSize', $("html").width() / 120 + "px");
+  // $("#roomSVG > svg").attr({
+  // 	'width': $("html").width() + 'px',
+  // 	'height': $("html").width() / 1.6 + 'px'
+  // }); //	For IE
+  //console.log($("html").width())
+  $("html").css('fontSize', $("html").width() / 120 + "px");
 }
 
 function loadNewRoom(roomName) {
 
-	$('#phaseNum').html(phaseText[phase]);
-	totalTriggers = 0;
-	$.getJSON(roomName + ".json", function(data) {
-		nextRoom = data.nextRoom;
+  $('#phaseNum').html(phaseText[phase]);
+  totalTriggers = 0;
+  $.getJSON(roomName + ".json", function(data) {
+    nextRoom = data.nextRoom;
 
-		clickable = data.targets;
-		//$("#room").css("font-size", "100px");
-		$("#roomSVG").load("img/" + data.roomImage, roomSvgLoad);
+    clickable = data.targets;
+    //$("#room").css("font-size", "100px");
+    $("#roomSVG").load("img/" + data.roomImage, roomSvgLoad);
 
-		resizeScreen();
-	})
+    resizeScreen();
+  })
 }
 
 
 function roomSvgLoad() {
 
-	$(clickable).each(function(index, value) {
-		console.log(value.Name)
-		if ("audioFile" in value) {
-			soundEffects[value.Name] = ss_soundbits("audio/" + value.audioFile);
-		}
-		if ("isTrigger" in value) {
+  $(clickable).each(function(index, value) {
+    console.log(value.Name)
+    if ("audioFile" in value) {
+      soundEffects[value.Name] = ss_soundbits("audio/" + value.audioFile);
+    }
+    if ("isTrigger" in value) {
 
-			if (phase == 1) {
-				var cubbyDiv = $( ".cubbyCopier" ).clone()
-				cubbyDiv.attr("id" ,  "cubby_" + value.Name)
-				cubbyDiv.attr("class",   "cubbyCopy")
-					$("#treasureChest").append(cubbyDiv)
-				var cubbySVG= $("#cubby_" + value.Name+" svg");
-
-
-
-				var cubbyItem =	jQuery("#" + value.Name).clone();
-
-				cubbyItem.attr("id", "cubbySVG_"+value.Name);
-				//cubbyItem.attr("transform","");
-				cubbyItem.appendTo(cubbySVG).css('opacity', '0');
-				cubbyItem.attr("transform","translate(1600,20) scale(1) ");
-				cubbyItemBBox=document.getElementById("cubbySVG_"+value.Name).getBBox()
-				//cubbySVG.attr("viewBox","0 0 "+cubbyItemBBox.width+" "+cubbyItemBBox.height);
-				console.log();
-
-				$("#cubbySVG_"+value.Name+' g[id]').each(function(item,val){
-
-var oldID = $(val).attr("id");
-$(val).attr("id","cubbySVG_"+oldID )
-$(val).attr("transform","");
+      if (phase == 1) {
+        var cubbyDiv = $(".cubbyCopier").clone()
+        cubbyDiv.attr("id", "cubby_" + value.Name)
+        cubbyDiv.attr("class", "cubbyCopy")
+        $("#treasureChest").append(cubbyDiv)
+        var cubbySVG = $("#cubby_" + value.Name + " svg");
 
 
-				})
 
-				//console.log(triggerItem);
+        var cubbyItem = jQuery("#" + value.Name).clone();
 
-				//$("body").append(cubbyDiv)
-			}
+        cubbyItem.attr("id", "cubbySVG_" + value.Name);
+        //cubbyItem.attr("transform","");
+        cubbyItem.appendTo(cubbySVG).css('opacity', '0');
+        cubbyItem.attr("transform", "translate(1600,20) scale(1) ");
+				console.log("cubbySVG_" + value.Name)
+        cubbyItemBBox = document.getElementById("cubbySVG_" + value.Name).getBBox()
+        //cubbySVG.attr("viewBox","0 0 "+cubbyItemBBox.width+" "+cubbyItemBBox.height);
+        console.log();
 
-			totalTriggers++;
-			value.unClicked = true;
-		}
-		$('.cubbyCopy').css("width",(100/totalTriggers)+"%")
-		triggersLeft = getParameterByName("triggersLeft") || totalTriggers;
+        $("#cubbySVG_" + value.Name + ' g[id]').each(function(item, val) {
 
-		displayTriggersLeft();
+          var oldID = $(val).attr("id");
+          $(val).attr("id", "cubbySVG_" + oldID)
+          $(val).attr("transform", "");
 
-		$("#" + value.Name).addClass("clickable")
-		lookup[value.Name] = index;
-		//startingWidth=$("#screen").width();
-		//startingHeight=$("#screen").height();
 
-		//aspect=startingWidth/startingHeight;
-		aspect=1/1
-		//console.log(aspect,$("#screen").height(),$("#roomSVG").height())
-	})
+        })
+
+        //console.log(triggerItem);
+
+        //$("body").append(cubbyDiv)
+      }
+
+      totalTriggers++;
+      value.unClicked = true;
+    }
+    $('.cubbyCopy').css("width", (100 / totalTriggers) + "%")
+    triggersLeft = getParameterByName("triggersLeft") || totalTriggers;
+
+    displayTriggersLeft();
+
+    $("#" + value.Name).addClass("clickable")
+    lookup[value.Name] = index;
+    //startingWidth=$("#screen").width();
+    //startingHeight=$("#screen").height();
+
+    //aspect=startingWidth/startingHeight;
+    aspect = 1 / 1
+    //console.log(aspect,$("#screen").height(),$("#roomSVG").height())
+  })
 
   /*
 	$(".cubbyCopy").css("width", (100/totalTriggers)+"%" )
@@ -172,215 +173,221 @@ $(val).attr("transform","");
 
 	}
 	*/
-	makeClickEvents();
+  makeClickEvents();
 
-	var item = getParameterByName("item") || "Intro"
+  var item = getParameterByName("item") || "Intro"
 
-	$('#' + item).trigger('click')
+  $('#' + item).trigger('click')
 
 
-	resizeScreen(); //	For IE
+  resizeScreen(); //	For IE
 }
 
 function makeClickEvents() {
 
-	$(".clickable").click(function(evt) {
-		console.log(evt)
+  $(".clickable").click(function(evt) {
+    console.log(evt)
 
-		var clickedItem = evt.currentTarget.id
-		if (clickedItem == "Door") {
-			if (!triggersLeft) {
-				changePhase();
-				loadNewRoom(nextRoom);
-				evt.preventDefault();
-				return;
-			}
-		}
-		// $(evt.target).closest('.clickable').attr("id");
-		itemClicked(clickedItem)
-	});
+    var clickedItem = evt.currentTarget.id
+    if (clickedItem == "Door") {
+      if (!triggersLeft) {
+        changePhase();
+        loadNewRoom(nextRoom);
+        evt.preventDefault();
+        return;
+      }
+    }
+    // $(evt.target).closest('.clickable').attr("id");
+    itemClicked(clickedItem)
+  });
 }
 
 function itemClicked(clickedItem) {
-	var item = clickable[lookup[clickedItem]];
+  var item = clickable[lookup[clickedItem]];
 
 
-	var fps = item.frameRate || defaultFrameRate;
-	if ("audioFile" in item) {
-		soundEffects[item.Name].playclip();
-	}
-	if ("isTrigger" in item) {
-		if (item.unClicked) {
-			countTriggers(item);
-		}
+  var fps = item.frameRate || defaultFrameRate;
+  if ("audioFile" in item) {
+    soundEffects[item.Name].playclip();
+  }
+  if ("isTrigger" in item) {
+    if (item.unClicked) {
+      countTriggers(item);
+    }
 
-	}
+  }
 
-	startAnimating(fps, item);
+  startAnimating(fps, item);
 
 
 };
 
 function disappear(it) {
-	var xerox = $("#cubbySVG_"+$(it).attr('id'));
-	it.animate({
-			opacity: 0,
-		},
-		1000,
-		function() {
+  var xerox = $("#cubbySVG_" + $(it).attr('id'));
+  it.animate({
+      opacity: 0,
+    },
+    1000,
+    function() {
 
 
-		}
-	);
-	xerox.animate({
-			opacity: 1,
-		},
-		1000,
-		function() {
+    }
+  );
+  xerox.animate({
+      opacity: 1,
+    },
+    1000,
+    function() {
 
 
-		}
-	);
+    }
+  );
 
 
 }
 
 
 function thoughts(it) {
-	$("#thoughtBubble").removeClass("thoughtPop");
+  $("#thoughtBubble").removeClass("thoughtPop");
 
 
 
 
 
-	var alreadyClickedText = ""
-	if (!it.unClicked && it.isTrigger) {
-		alreadyClickedText = " <em>(You found this already!)</em>"
-	}
+  var alreadyClickedText = ""
+  if (!it.unClicked && it.isTrigger) {
+    alreadyClickedText = " <em>(You found this already!)</em>"
+  }
 
 
-	displayThought()
-	it.unClicked = false;
-	$("#close").click(function(evt) {
+  displayThought()
+  it.unClicked = false;
+  $("#close").click(function(evt) {
+
 
 		$("#thoughtBubble").css({
-			"display": "none"
-		});
+      "display": "none"
+    });
 
-	});
-
-	function displayThought() {
-
-		$("#thoughtBubble").css({
-			"left": it.xValue + "rem",
-			"top": it.yValue + "rem"
-		});
-		$("#thoughtBubble").addClass("thoughtPop");
-		$("#thoughtBubble").css("display", "inline");
-
-		if (getParameterByName("edit")) {
-
-			$('#thoughtBubble p').html('<form><textarea id=txtArea></textarea> </form>');
-
-
-			$('textarea#txtArea').ckeditor({
-				height: "300px",
-				toolbarStartupExpanded: true,
-				width: "100%"
-			});
-
-			$('textarea').val(it.Text[phase])
-
-			$('form').submit(function(event) {
-
-				$.post("save.php", {
-					name: it.Name,
-					room: currentRoom,
-					phase: phase,
-					text: $('textarea').val()
-				}).done(function(data) {
-					window.location = "?edit=True&item=" + it.Name + "&room=" + currentRoom + "&phase=" + phase;
-					///  window.location = window.location.search
-				});
-				console.log("" || it.Name)
-				event.preventDefault();
-			})
-
-
-
-		} else {
-			$("#thoughtBubble p").html(it.Text[phase] + alreadyClickedText)
+		if (phase == 1 && it.isTrigger) {
+			disappear($("#" + it.Name));
 		}
 
-	}
+
+  });
+
+  function displayThought() {
+console.log(it.xValue||50 )
+    $("#thoughtBubble").css({
+      "left": it.xValue||50 + "%",
+      "top": it.yValue||50  + "%"
+    });
+    $("#thoughtBubble").addClass("thoughtPop");
+    $("#thoughtBubble").css("display", "inline");
+
+    if (getParameterByName("edit")) {
+
+      $('#thoughtBubble p').html('<form><textarea id=txtArea></textarea> </form>');
+
+
+      $('textarea#txtArea').ckeditor({
+        height: "300px",
+        toolbarStartupExpanded: true,
+        width: "100%"
+      });
+
+      $('textarea').val(it.Text[phase])
+
+      $('form').submit(function(event) {
+
+        $.post("save.php", {
+          name: it.Name,
+          room: currentRoom,
+          phase: phase,
+          text: $('textarea').val()
+        }).done(function(data) {
+          window.location = "?edit=True&item=" + it.Name + "&room=" + currentRoom + "&phase=" + phase;
+          ///  window.location = window.location.search
+        });
+        console.log("" || it.Name)
+        event.preventDefault();
+      })
+
+
+
+    } else {
+      $("#thoughtBubble p").html(it.Text[phase] + alreadyClickedText)
+    }
+
+  }
 
 
 }
 
 function countTriggers(item) {
-	if (triggersLeft) triggersLeft--;
-	displayTriggersLeft();
+  if (triggersLeft) triggersLeft--;
+  displayTriggersLeft();
 }
 
 function changePhase() {
-	phase = phase == 0 ? 1 : 0;
+  phase = 1 - phase
 }
 
 
 
 function displayTriggersLeft() {
 
-	$('#triggersLeft').html(triggersLeft);
+  $('#triggersLeft').html(triggersLeft);
 }
 // initialize the timer variables and start the animation
 function startAnimating(fps, item) {
-	animationItem = item;
-	animationItem.animationFrame = 1;
-	animationItem.loopAmount = item.loopAmount | 0;
-	animatingList.push(animationItem)
-	fpsInterval = 1000 / fps;
+  if (!isAnimating) {
+    isAnimating = true;
+    animationItem = item;
+    animationItem.animationFrame = 1;
+    animationItem.loopAmount = item.loopAmount | 0;
+    animatingList.push(animationItem)
+    fpsInterval = 1000 / fps;
 
-	then = Date.now();
-	startTime = then;
-	animate();
+    then = Date.now();
+    startTime = then;
+    animate();
+  }
 }
 
 function animate() {
+  if (isAnimating) requestAnimationFrame(animate);
+  // calc elapsed time since last loop
+  now = Date.now();
+  elapsed = now - then;
+  // if enough time has elapsed, dr)aw the next frame
+  if (elapsed > fpsInterval) {
+    then = now - (elapsed % fpsInterval);
+    animatingList.forEach(function(item, index) {
 
-	// request another frame
-	isAnimating = requestAnimationFrame(animate);
-	// calc elapsed time since last loop
-	now = Date.now();
-	elapsed = now - then;
-	// if enough time has elapsed, dr)aw the next frame
-	if (elapsed > fpsInterval) {
-		then = now - (elapsed % fpsInterval);
-		animatingList.forEach(function(item, index) {
-			console.log(item)
-			var itemID = item.Name.split("_")[0] + "_";
-			var selector = "#" + itemID;
-			$("[id^='" + itemID + "']").attr("style", "display:none")
-			var animationLength = $("[id^='" + itemID + "']").length;
-			//console.log(y);
-			if (item.animationFrame <= (animationLength * (animationItem.loopAmount + 1))) {
-				//var displayFrame = (animationFrame % animationItem.totalAnimationFrames) + 1
-				//wacky cal
-				var displayFrame = Math.abs((item.animationFrame + animationLength - 2) % ((animationLength - 1) * 2) - (animationLength - 1)) + 1
+      var itemID = item.Name.split("_")[0] + "_";
+      var selector = "#" + itemID;
+      $("[id^='" + itemID + "']").attr("style", "display:none")
+      var animationLength = $("[id^='" + itemID + "']").length;
+      //console.log(y);
+      if (item.animationFrame <= (animationLength * (animationItem.loopAmount + 1))) {
+        //var displayFrame = (animationFrame % animationItem.totalAnimationFrames) + 1
+        //wacky cal
+        var displayFrame = Math.abs((item.animationFrame + animationLength - 2) % ((animationLength - 1) * 2) - (animationLength - 1)) + 1
 
-				$(selector + displayFrame).attr("style", "display:inline")
-				item.animationFrame++
-			} else {
-				thoughts(item);
-				delete animatingList[index]
-				if (item.stopAtEnd) {
-					$(selector + animationLength).attr("style", "display:inline");
-				} else {
-					$(selector + 1).attr("style", "display:inline");
-				}
-				if (phase == 1 && item.isTrigger) {
-					disappear($("#" + item.Name));
-				}
-			}
-		});
-	}
+        $(selector + displayFrame).attr("style", "display:inline")
+        item.animationFrame++
+      } else {
+        thoughts(item);
+        delete animatingList[index]
+        isAnimating = false;
+        if (item.stopAtEnd) {
+          $(selector + animationLength).attr("style", "display:inline");
+        } else {
+          $(selector + 1).attr("style", "display:inline");
+        }
+
+      }
+    });
+  }
 }
